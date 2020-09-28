3b6687446c13990855166f6513151bdf87aebfd8
Kejoko <keeganjkochis@gmail.com>
2020-09-28 11:54:19 -0600

    Create window

--- juniper/EntryPoint.cpp
+++ juniper/EntryPoint.cpp
@@ -8,7 +8,36 @@
 
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, from Juniper v2!\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* pJuniperWindow = glfwCreateWindow(800, 600, "Juniper Engine", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(pJuniperWindow)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(pJuniperWindow);
+
+    glfwTerminate();
+
     return 0;
 }
